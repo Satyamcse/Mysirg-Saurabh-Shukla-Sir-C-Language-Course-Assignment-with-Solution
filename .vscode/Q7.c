df2a54947f221b3abb1ed6718fc354e351b5c1d4
Satyam Tomar <125190648+Satyamcse@users.noreply.github.com>
2024-03-07 14:13:47 +0530

    Delete .vscode directory

--- .vscode/Q7.c
+++
@@ -1,9 +0,0 @@
-// Write a program which takes a character as an input and displays its ASCII code.
-#include<stdio.h>
-int main(){
-    char m;
-    printf("Enter Character");
-    scanf("%c",&m);
-    printf("%d",m);
-    return 0;
-}
